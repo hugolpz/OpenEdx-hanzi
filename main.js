7a2acf853833e4be15c6de790ebfe5dbc5f1415c
hugolpz <hugo.lpz@gmail.com>
2018-02-02 11:07:30 +0000

    minor fix (distinction of duplicata via lesson number in id)

--- main.js
+++ main.js
@@ -7,16 +7,14 @@ var player = [],
 		writer = [],
 		audios = [];
 
+/* *********************************************************************** */
+/* DATA LOADING APPROACH ************************************************* */
 var voc;
 $.ajax({ type: 'GET', data: {},
     url: "./vocabulary.json", dataType: 'json',
     success: function(data) { voc = data; },
     async: false 
 });
-
-
-/* *********************************************************************** */
-/* DATA LOADING APPROACH ************************************************* */
 var opts = {
 	player : { width: 150, height: 150, padding: 5,strokeColor:"#333" },
 	writer : { width: 300, height: 300, padding: 5,strokeColor:"#333",
@@ -38,26 +36,26 @@ if (dataLoadingApproach === 'local'){
 /* *********************************************************************** */
 /* TOOLBOX *************************************************************** */
 var injectStrokeOrderDisplay = function(elementSelector,item) {
-	var hans = item.hans, pin1yin1 = item.pinyin;
-	var elementPlayer='<div id="play'+hans+'" class="play zi" zi="'+hans+'"></div>';
+	var lesson = item.lesson.toString(), hans = item.hans, pin1yin1 = item.pinyin;
+	var elementPlayer='<div id="playL'+lesson+hans+'" class="play zi" zi="'+hans+'"></div>';
 	$(elementSelector).append(elementPlayer);
-	player[hans] = new HanziWriter("play"+hans, hans, opts.player);
+	player[hans] = new HanziWriter('playL'+lesson+hans, hans, opts.player);
 };
 
 var injectStrokeOrderQuiz = function(elementSelector,item) {
-	var hans = item.hans, pin1yin1 = item.pinyin;
-	var elementWriterGlyph ='<div id="write'+hans+'" class="write zi" zi="'+hans+'"></div>',
-			elementWriterResetButton = '<div id="writeReset'+hans+'" class="writeReset zi" zi="'+hans+'">Reset '+hans+'</div>';
+	var lesson = item.lesson.toString(), hans = item.hans, pin1yin1 = item.pinyin;
+	var elementWriterGlyph ='<div id="writeL'+lesson+hans+'" class="write zi" zi="'+hans+'"></div>',
+			elementWriterResetButton = '<div id="writeResetL'+lesson+hans+'" class="writeReset zi" zi="'+hans+'">Reset '+hans+'</div>';
 	$(elementSelector).append(elementWriterGlyph+elementWriterResetButton);
-	writer[hans] = new HanziWriter("write"+hans, hans, opts.writer);
+	writer[hans] = new HanziWriter('writeL'+lesson+hans, hans, opts.writer);
 	writer[hans].quiz({onComplete: function(d){ console.log(d);}});
 };
 
 var injectAudio = function(elementSelector,item){
-	var hans = item.hans, pin1yin1 = item.pin1yin1.replace("5", "1");
+	var lesson = item.lesson.toString(), hans = item.hans, pin1yin1 = item.pin1yin1.replace("5", "1");
 	var sound = new Howl({ src: [ 'https://raw.githubusercontent.com/hugolpz/audio-cmn/master/64k/syllabs/cmn-'+pin1yin1+'.mp3', 'https://raw.githubusercontent.com/hugolpz/audio-cmn/master/64k/hsk/cmn-'+hans+'.mp3']});
   audios[hans] = sound;
-	var elementAudioButton = '<div id="audio'+hans+'" class="audio zi" zi="'+hans+'">Play '+hans+'</div>';
+	var elementAudioButton = '<div id="audioL'+lesson+hans+'" class="audio zi" zi="'+hans+'">Play '+hans+'</div>';
 	$(elementSelector).append(elementAudioButton);
 };
 /* *********************************************************************** */
