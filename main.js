6760d9da55c38a74f689baff866305e4fb1986bb
hugolpz <hugo.lpz@gmail.com>
2018-02-05 15:29:08 +0000

    edit index and main.js so to include bulma

--- main.js
+++ main.js
@@ -13,12 +13,13 @@ var voc;
 $.ajax({ type: 'GET', data: {},
     url: "./vocabulary.json", dataType: 'json',
     success: function(data) { voc = data; },
-    async: false 
+    async: false
 });
 var opts = {
-	player : { width: 150, height: 150, padding: 5,strokeColor:"#333" },
-	writer : { width: 300, height: 300, padding: 5,strokeColor:"#333",
-						showCharacter: false, showHintAfterMisses: 1, 
+	player : { width: 96, height: 96, padding: 5, 
+						strokeColor:"#333", radicalColor: '#660000' },
+	writer : { width: 300, height: 300, padding: 5, strokeColor:"#333", drawingWidth: 20,
+						showCharacter: false, showHintAfterMisses: 1,
 						ighlightOnComplete: true, highlightColor: "#B16666" },
 	localLoader : { charDataLoader: function(char, onComplete) {
     $.getJSON("/" + char + ".json", function(charData) { onComplete(charData); });
@@ -27,67 +28,146 @@ var opts = {
 
 var dataLoadingApproach = 'API'; 
 if (dataLoadingApproach === 'local'){
-	for (var attrname in opts.localLoader) { 
+	for (var attrname in opts.localLoader) {
 		opts.player[attrname] = opts.localLoader[attrname];
 		opts.writer[attrname] = opts.localLoader[attrname];
 	}
 }
 
 /* *********************************************************************** */
-/* TOOLBOX *************************************************************** */
-var injectStrokeOrderDisplay = function(elementSelector,item) {
-	var lesson = item.lesson.toString(), hans = item.hans, pin1yin1 = item.pinyin;
-	var elementPlayer='<div id="playL'+lesson+hans+'" class="play zi" zi="'+hans+'"></div>';
-	$(elementSelector).append(elementPlayer);
-	player[hans] = new HanziWriter('playL'+lesson+hans, hans, opts.player);
-};
+/* CMNCARD *************************************************************** */
+var cardTpl = function(item, i) { 
+	var lesson = 'L'+item.lesson, hans = item.hans, pin1yin1 = item.pin1yin1; //.replace("5", "1");
+	var key = lesson+hans+i;
+	var tpl = `
+		<div class="card `+lesson+`" lesson="`+lesson+`" zi="`+hans+`" i="`+i+`">
+			<div class="card-image">
+				<figure class="image is-4by4">
+					<div id="write`+key+`" class="writer"></div>
+				</figure>
+			</div>
 
-var injectStrokeOrderQuiz = function(elementSelector,item) {
-	var lesson = item.lesson.toString(), hans = item.hans, pin1yin1 = item.pinyin;
-	var elementWriterGlyph ='<div id="writeL'+lesson+hans+'" class="write zi" zi="'+hans+'"></div>',
-			elementWriterResetButton = '<div id="writeResetL'+lesson+hans+'" class="writeReset zi" zi="'+hans+'">Reset '+hans+'</div>';
-	$(elementSelector).append(elementWriterGlyph+elementWriterResetButton);
-	writer[hans] = new HanziWriter('writeL'+lesson+hans, hans, opts.writer);
-	writer[hans].quiz({onComplete: function(d){ console.log(d);}});
-};
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-4by4 is-centered">
+							<div id="play`+key+`" class="play"></div>
+						</figure>
+					</div>
+					<div class="media-content play audio">
+						<p class="title is-4">`+hans+` `+item.pinyin+`</p>
+						<p class="subtitle is-6">`+item.definition+`</p>
+					</div>
+				</div>
 
-var injectAudio = function(elementSelector,item){
-	var lesson = item.lesson.toString(), hans = item.hans, pin1yin1 = item.pin1yin1.replace("5", "1");
-	var sound = new Howl({ src: [ 'https://raw.githubusercontent.com/hugolpz/audio-cmn/master/64k/syllabs/cmn-'+pin1yin1+'.mp3', 'https://raw.githubusercontent.com/hugolpz/audio-cmn/master/64k/hsk/cmn-'+hans+'.mp3']});
-  audios[hans] = sound;
-	var elementAudioButton = '<div id="audioL'+lesson+hans+'" class="audio zi" zi="'+hans+'">Play '+hans+'</div>';
-	$(elementSelector).append(elementAudioButton);
-};
+				<div class="content">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+					<a href="#">#`+lesson+`</a>
+					<br>
+					<span class="icon is-small"><i class="fas fa-retweet"></i></span>
+				</div>
+			</div>
+
+			<footer class="card-footer">
+				<a class="card-footer-item play">Ecriture</a>
+				<a class="card-footer-item audio">Audio</a>
+				<a class="card-footer-item writeReset">Effacer mon pinceau</a>
+			</footer>
+		</div>`;
+	return tpl;
+}
+
+
+
 /* *********************************************************************** */
-/* INJECTIONS ************************************************************ */
-for (var i=0;i<voc.length; i++){
-	var item = voc[i];
-	var hans = item.hans;
-	if (hans.length == 1) {
-		injectStrokeOrderDisplay(".list.play", item);
-		injectStrokeOrderQuiz(".list.write", item);
-		injectAudio(".list.audio",item);
+/* TOOLBOX *************************************************************** 
+var injectStrokeOrderDisplay = function(elementSelector,item, i) {
+	var lesson = 'L'+item.lesson, hans = item.hans, pin1yin1 = item.pinyin;
+	var key = lesson+hans+i;
+	var elementPlayer='<div id="play'+key+'" class="play zi '+lesson+'" lesson="'+lesson+'" zi="'+hans+'" i="'+i+'"></div>';
+	$(elementSelector).append(elementPlayer);
+	player[key] = new HanziWriter('play'+key, hans, opts.player);
+};
+
+var injectStrokeOrderQuiz = function(elementSelector,item, i) {
+	var lesson = 'L'+item.lesson, hans = item.hans, pin1yin1 = item.pinyin;
+	var key = lesson+hans+i;
+	var elementWriterGlyph ='<div id="write'+key+'" class="write zi '+lesson+'" lesson="'+lesson+'" zi="'+hans+'" i="'+i+'"></div>',
+			elementWriterResetButton = '<div id="writeReset'+key+'" class="writeReset zi '+lesson+'" lesson="'+lesson+'" zi="'+hans+'" i="'+i+'">Reset '+hans+'</div>';
+	$(elementSelector).append(elementWriterGlyph+elementWriterResetButton);
+	writer[lesson+hans+i] = new HanziWriter('write'+key, hans, opts.writer);
+	writer[lesson+hans+i].quiz({onComplete: function(d){ console.log(d);}});
+};
+
+var injectAudio = function(elementSelector,item, i){
+	var lesson = 'L'+item.lesson, hans = item.hans, pin1yin1 = item.pin1yin1.replace("5", "1");
+	var key = lesson+hans+i;
+	var sound = new Howl({ src: [ 'https://raw.githubusercontent.com/hugolpz/audio-cmn/master/64k/syllabs/cmn-'+pin1yin1+'.mp3', 'https://raw.githubusercontent.com/hugolpz/audio-cmn/master/64k/hsk/cmn-'+hans+'.mp3']});
+  audios[lesson+hans+i] = sound;
+	var elementAudioButton = '<div id="audio'+key+'" class="audio zi '+lesson+'" lesson="'+lesson+'" zi="'+hans+'" i="'+i+'">Play '+hans+'</div>';
+	$(elementSelector).append(elementAudioButton);
+}; */
+/* *********************************************************************** */
+/* INJECTIONS ************************************************************ 
+var injectMultimedia = function(item,i) {
+	if (item.hans.length == 1) {
+		injectStrokeOrderDisplay(".list.play", item, i);
+		injectStrokeOrderQuiz(".list.write", item, i);
+		injectAudio(".list.audio",item, i);
 	}
+} */
+var injectMultimedia = function (item,i) {
+	var lesson = 'L'+item.lesson, hans = item.hans, pin1yin1 = item.pin1yin1; //.replace("5", "1");
+	var key = lesson+hans+i;
+	
+	//Inject HTML
+	var cardHTML = cardTpl(item,i);
+	$('#hook').append(cardHTML)
+	
+	// Inject and assing activities to arrays
+	// Player (Stroke Order)
+	player[key] = new HanziWriter('play'+key, hans, opts.player); 
+	// Writer
+	writer[key] = new HanziWriter('write'+key, hans, opts.writer);
+	writer[key].quiz({onComplete: function(d){ console.log(d);}});
+	// Audio
+	var audioRoot = 'https://raw.githubusercontent.com/hugolpz/audio-cmn/master/64k';
+	var sound = new Howl({ src: [ audioRoot+'/syllabs/cmn-'+pin1yin1+'.mp3', audioRoot+'/hsk/cmn-'+hans+'.mp3']});
+  audios[key] = sound;
+}
+
+
+for (var i=0;i<voc.length; i++){
+	if (voc[i].hans.length == 1) { injectMultimedia(voc[i],i) }
 }
 //console.log(player);
 //console.log(writer);
 //console.log(audios);
 
+
 /* *********************************************************************** */
 /* INTERACTIONS ********************************************************** */
 $('.play').on('click', function() {  
-	var zi = $(this).attr("zi");
+	var $parentCard = $(this).closest('.card');
+	var key = $parentCard.attr('lesson') + $parentCard.attr('zi') + $parentCard.attr('i');
 	// console.log(zi+"dfvdrf",JSON.stringify(player[zi][0]));
-	player[zi].animateCharacter();
-	console.log('.play',zi);
+	player[key].animateCharacter();
+	console.log('.play',key);
 });
 $(".writeReset").on('click', function(){ 
-	var zi= $(this).attr("zi");
-	writer[zi].quiz({});
-	console.log('#writeReset',zi);
+	var $parentCard = $(this).closest('.card');
+	var key = $parentCard.attr('lesson') + $parentCard.attr('zi') + $parentCard.attr('i');
+	writer[key].quiz({});
+	console.log('#writeReset',key);
 }); /**/
 $('.audio, .play').on('click', function() {  
-	var zi = $(this).attr("zi") || $(this).data("zi");
-	audios[zi].play();
-	console.log('.audio',zi);
+	var $parentCard = $(this).closest('.card');
+	var key = $parentCard.attr('lesson') + $parentCard.attr('zi') + $parentCard.attr('i');
+	//var id = audios[key]._sounds._id;
+	// console.log('sound',id);
+	audios[key].play();
+//	audios[key].fade(0,1,100);
+//	audios[key].fade(1,0,1000);
+	console.log('.audio',key);
 }); /**/
